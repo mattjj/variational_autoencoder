6f0f484ea6260d8f3046269a9935af091d565950
Matthew Johnson <mattjj@csail.mit.edu>
2016-01-08 13:26:28 -0500

    add letters experiment and make_binary_fitter (untested)

---
+++ experiments/letters.py
@@ -0,0 +1,29 @@
+from __future__ import division
+import numpy as np
+import numpy.random as npr
+import cPickle as pickle
+import string
+import gzip
+
+from vae.vae import make_binary_fitter
+from vae.optimization import adadelta
+from vae.util import get_ndarrays, floatX
+
+from load import load_letters
+
+
+if __name__ == "__main__":
+    logging.info('\n\nStarting experiment!')
+    npr.seed(0)
+
+    images, labels = load_letters()
+    trX = floatX(images[labels == string.lowercase.index('g')])
+
+    encoder_params, decoder_params, fit = make_binary_fitter(trX, 3, [200], [200])
+
+    fit(1, 50, 1, adadelta())
+    fit(1, 250, 1, adadelta())
+
+    params = get_ndarrays(encoder_params), get_ndarrays(decoder_params)
+    with gzip.open('letter_params.pkl.gz', 'w') as f:
+        pickle.dump(params, f, protocol=-1)
